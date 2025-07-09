e78ec23e44a396817684fe9a862012fa5915cf9e
Aleksandr Hartov <a.hartov152@mail.ru>
2025-07-09 20:43:24 +0700

    Delete unused elem from adminlte, Add icons, Create layouts

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', AdminController::class);
+Route::get('/', IndexController::class)->name('main.index');
